befcbe7bcde4e708f4ba41e7243501273f01d81e
diego boya <dboyatjian@gmail.com>
2021-04-14 20:29:09 -0300

    pregunta 1 resuelta

--- pregunta1.js
+++ pregunta1.js
@@ -3,13 +3,24 @@
  * solo los numeros pares y solo numeros impares respectivamente
  */
 
+//pusheando los elementos a un nuevo array
+const arrayPares = [];
 const even = function(numbers){
+    numbers.forEach(number => {
+        if(number%2 === 0){
     
+        arrayPares.push(number);
+    }  
+    })
+    return arrayPares;
 }
 
+//utilizando filter
 const odd = function(numbers){
-
+    const impares = numbers.filter( number => number%2 !== 0);
+    return impares;
 }
+
 
 // TESTS (no modificar)
 const arrEqual = (a1,a2) => JSON.stringify(a1) == JSON.stringify(a2);
