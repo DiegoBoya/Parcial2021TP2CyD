15bdc62bc588732a1cd74af58b99e3a9e3d87d17
diego boya <dboyatjian@gmail.com>
2021-04-14 21:47:34 -0300

    pregunta 3 ok, Boyatjian Diego

--- pregunta3.js
+++ pregunta3.js
@@ -2,10 +2,23 @@
 // Por ejemplo en: [4,3,4,5,1] el tercer mayor es 3
 // si la longitud del array es < 3 se debe retornar undefined
 
-const greater3 = nums => {
+
+const greater3 = numbers => {
     
+//ordenamiento ascendente de los elementos de larray
+numbers.sort((a,b) => a - b );
+
+//set no guarda elementos repetidos
+const sinRep = [new Set(numbers)];
+
+
+if (sinRep.lenght < 3 ||numbers.lenght < 3 ){
+    return undefined;
 }
 
+return sinRep[2];
+
+}
 
 // TESTs no modificar
 console.log(greater3([4,3,4,5,1]) === 3);
